316b0802f0ca403aa00ab5042143b0d525388ed6
lulingling <1532229668@qq.com>
2016-02-18 15:10:42 +0800

    index html

---
+++ website/multicolor/js/main.js
@@ -0,0 +1,26 @@
+jQuery(function ($) {
+    'use strict';
+
+    /*Responsive Nav*/
+    $("li.dropdown").find(".fa-angle-down").each(function () {
+        $(this).click(function () {
+            if ($(window).width() < 768) {
+                $(this).parent().next().slideToggle();
+            }
+            return false;
+        })
+    });
+
+    /*Search form hide and show*/
+    $(".fa-search").click(function () {
+        $(".field-toggle").toggle();
+    });
+
+    /*Init wow js*/
+    new WOW().init();
+
+    $(window).load(function () {
+        $(".main-slider").addClass("animate-in");
+        $(".preloader").remove();
+    })
+});
